a7c4228e7f4a47fb8a644733ee69d685078ed5e9
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-07-30 21:39:25 -0500

    projectInit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("trii JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/trii";
+
+module.exports = MONGO_URI;
