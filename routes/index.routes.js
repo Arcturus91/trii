b32b8cbd8e7c903dd15687bdaa2b425323183d1c
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-07-30 22:35:52 -0500

    stockPricest

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
+const axios = require('axios').default;
+/* var request = require('request'); */
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
+
+router.get("/ftnt", (req, res, next) => {
+  
+  axios.get(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=KDMP5TP4UWMJKYWX")
+     .then(data => {
+      let jsonData = data.data;
+      let value = jsonData['Time Series (Daily)']
+      let price = Object.values(value)
+
+
+      console.log("ya tengo el array ", price[99]['4. close']
+      )
+      
+    res.send("hola")
+    })
+     .catch(err => console.log(err));
+})
+
+
 
 module.exports = router;
