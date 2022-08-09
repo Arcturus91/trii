0327c8abf3c565421fe43e48bb33e6d1e736f2e7
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-08-08 19:12:40 -0500

    axios get solved

--- public/js/portfolioGraph.js
+++ public/js/portfolioGraph.js
@@ -1,20 +1,30 @@
+
+//const vantageKey = process.env.VANTAGE_KEY;
+
+//Consultas:
+/* Cómo agregar un key tipo process aquí? */
+/* Cómo order el flow del api request para tener la data cuando la necesite. Necesito hacerlo síncrono.*/
+/* Cuando borro el item del database directamente, me deja un undefined en ese instante */
+
 const ctx = document.getElementById("myChart2").getContext("2d");
 const betaHtml = document.getElementById("betaPortfolioID")
 
 let url = window.location.href;
 
+//setting up userId
 let startPos = url.indexOf("profile/") + 8;
 let userId = url.slice(startPos, startPos + 24).toUpperCase(); //24 is MONGO ID object length
 
+//setting up URL base
 if (url.indexOf("localhost:3000") > -1) {
-  routeBeginning = "localhost:3000";
+  routeBeginning = "http://localhost:3000";
 } else {
-  routeBeginning = "trii-peru.herokuapp.com";
+  routeBeginning = "https://trii-peru.herokuapp.com";
 }
 
 const pullStocks = () => {
   axios
-    .get(`http://${routeBeginning}/auth/profile/${userId}/stocks`)
+    .get(`${routeBeginning}/auth/profile/${userId}/stocks`)
     .then((data) => {
       const stocksArray = data.data;
 
@@ -54,9 +64,9 @@ const pullStocks = () => {
 
       stocksNames.forEach((element, idx) => {
         
-//am adding a setTimeout in case, am making to many requests so fast that the api rejects the call.
-        setTimeout(function() {
+
           portfolio[element] = stockValue[idx] / sumPortfolio;
+          console.log("el portfolio ", portfolio)
           axios
           .get(
             `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${element}&apikey=KDMP5TP4UWMJKYWX`
@@ -64,10 +74,14 @@ const pullStocks = () => {
           .then((rawData) => {
             jsonData = rawData.data;
             stocksBeta[jsonData.Symbol] = Number(jsonData.Beta);
-
+            console.log("soy stocks beta", stocksBeta )
             if (idx === stocksNames.length - 1) {
               for (const key in portfolio) {
+
+//aqui se podría agregar lo del error por si hay error en beta.
+
                 betaPort += portfolio[key] * stocksBeta[key];
+                console.log("yo soy beta port", betaPort)
               }
               betaHtml.value = 'El Beta de tu portfolio es: '+ betaPort.toFixed(2)
 
@@ -79,14 +93,6 @@ if(betaPort > 0){betaHtml.type = "text"}
               console.log("json data del beta get", stocksBeta, betaPort);
             }
           });
-
-
-        }, 2000);
-
-
-
-
-
 
       });
     })
